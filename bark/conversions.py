5d2daf0638219e7aa48b1e18471ef98513e936fb
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-12-17 12:17:41 -0600

    Use zip in Format.convert() loop.

--- bark/conversions.py
+++ bark/conversions.py
@@ -223,10 +223,10 @@ class Format(object):
         """
 
         result = []
-        for idx, conv in enumerate(self.conversions):
+        for conv, datum in zip(self.conversions, data):
             # Only include conversion if it's allowed
             if conv.modifier.accept(response.status_code):
-                result.append(conv.convert(request, response, data[idx]))
+                result.append(conv.convert(request, response, datum))
             else:
                 result.append('-')
 
